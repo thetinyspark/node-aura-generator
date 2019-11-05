4c3ea65952e2ae4ac86e9fcfe07d41e8b70ddd9c
thetinyspark <contact@the-tiny-spark.com>
2019-11-05 23:28:05 +0100

    add a Fireball class & modify some config

--- src/aura.js
+++ src/aura.js
@@ -1,83 +1,3 @@
-/**
- * 
- * **********************************************************************
- * ************************ MATH FUNCTIONS **********************
- * **********************************************************************
- */
-
-function straight(t, m, o = 0, p = 1, b = 12) {
-    t = t % m;
-
-    let r1 = doubleSawtooth(t, m, 0);
-
-    return r1;
-    // return Math.pow(r1, p);
-}
-
-function linear(t, m, r = 90) {
-
-    return t / m;
-}
-
-
-function teeth(t, m, o = 0, p = 1, b = 12) {
-    t = t % m;
-
-    let num = 0;
-    let percent = t / m;
-    let offset = (percent < 0.5) ? -o : o;
-    let r1 = doubleSawtooth(t, m, 0);
-    let r2 = sawtooth(t * b, m, offset);
-    let r3 = pseudorandom(t * m);
-
-    if (t < m / 2) {
-        r2 = 1 - r2;
-    }
-
-    num = 0.8 * r1;
-    num += 0.1 * r2;
-    num += 0.1 * r3;
-    return Math.pow(num, p);
-}
-
-function round(t, m, r = 90) {
-    t = t % m;
-    let start = 270 - r;
-    let angle = start + (r * 2 * t / m);
-    let num = Math.cos((angle % 360) * Math.PI / 180);
-
-    return num;
-}
-
-function pseudorandom(num, seed = 0) {
-
-    let t = seed + ((num % 0xFFFF) * 0x6D2B79F5);
-
-    t = Math.imul(t ^ t >>> 15, t | 1);
-    t ^= t + Math.imul(t ^ t >>> 7, t | 61);
-    return ((t ^ t >>> 14) >>> 0) / 4294967296;
-}
-
-
-// fonction traçant une courbe de type "dent de scie" au cours du temps
-// le paramètre "t" représente le "temps courant"
-// le paramètre "m" représente le "temps maximum"
-// le paramètre "o" représente un décalage dans le temps "offset"
-function sawtooth(t, m, o = 0) {
-    let x = (t + o) / m;
-    return (1 - x - Math.floor(1 - x));
-}
-
-// fonction traçant une courbe de type "dent de scie" au cours du temps
-// le paramètre "t" représente le "temps courant"
-// le paramètre "m" représente le "temps maximum"
-// le paramètre "o" représente un décalage dans le temps "offset"
-function doubleSawtooth(t, m, o = 0) {
-    let x = (t + o) / m * 2;
-    return 1 - Math.abs(x % 2 - 1);
-}
-
-
 
 
 /**
@@ -86,116 +6,74 @@ function doubleSawtooth(t, m, o = 0) {
  * *****************************************************************
  */
 
-
+const {Fireball} = require('./Fireball');
 const { createCanvas } = require('canvas');
 const fs = require("fs");
 const Param = require("./utils/params");
 let canvas = null;
 let ctx = null;
 
-function drawAura(context, spike, config, time, duration) {
-
-
-    let i = 0;
-    let x = 0;
-    let y = 0;
-
-    let peaks = parseInt(config.peaks);
-    let power = parseInt(config.power);
-    let lineCap = config.lineCap;
-    let lineWidth = config.lineWidth;
-    let shadowBlurX = parseFloat(config.shadowBlurX);
-    let shadowBlurY = parseFloat(config.shadowBlurY);
-    let isRound = config.round == true;
-    let isStraight = config.straight == true;
-    let lineColor = config.lineColor;
-    let shadowColor = config.shadowColor;
-    let innerColor = config.innerColor;
-    let sx = parseInt(spike.x);
-    let sy = parseInt(spike.y);
-    let width = parseInt(spike.width) >> 1;
-    let height = parseInt(spike.height);
-    let rot = parseInt(spike.rot) * Math.PI / 180;
-    let blend = spike.blend;
-
-    let step = width * 2 / duration;
-
-    context.save();
-
-    context.translate(sx, sy);
-    context.rotate(rot);
-    context.globalCompositeOperation = blend;
-
-    context.beginPath();
-
-    context.shadowBlurX = shadowBlurX;
-    context.shadowBlurY = shadowBlurY;
-    context.shadowColor = shadowColor;
-    context.lineWidth = lineWidth;
-    context.lineCap = lineCap;
-    context.strokeStyle = lineColor;
-    context.fillStyle = innerColor;
-
-    context.moveTo(-width, 0);
-
-
-    for (i = 0; i < duration; i++) {
-
-        if (isRound) {
-            x = (round(i, duration) * width);
-        }
-        else {
-            x = (i * step) - width;
-        }
-
-        if (isStraight) {
-            y = -(straight(i, duration, time, power, peaks)) * height;
-        }
-        else {
-            y = -(teeth(i, duration, time, power, peaks)) * height;
-        }
-
-        context.lineTo(x, y);
-    }
-
-    context.lineTo(width, 0);
-    context.moveTo(0, 0);
-    context.arc(0, 0, width, 0, Math.PI);
-
-
-    context.stroke();
-    context.fill();
-    context.closePath();
-
-    context.restore();
-
-}
-
 function init(config) {
 
+    let offset = 0;
+    let offsetStep = parseInt(config.offsetStep);
     let width = parseInt(config.width);
     let height = parseInt(config.height);
-    let i = 0;
-    let duration = parseInt(config.duration);
-    let step = parseInt(config.step);
-    let time = parseInt(config.time);
+    let numFrames = parseInt(config.numFrames);
     let out = config.out;
+    let fire = new Fireball(1,1);
+    let ball = null;
 
     canvas = createCanvas(width, height);
     canvas.width = width;
     canvas.height = height;
     ctx = canvas.getContext('2d');
 
-    for (let j = time; j < duration; j += step) {
-        ctx.clearRect(0, 0, width, height);
-        ctx.save();
+    for (let j = 0; j < numFrames; j ++) {
+
         out = config.out.replace("{{$index}}", j);
 
-        for (i = 0; i < config.spikes.length; i++) {
-            drawAura(ctx, config.spikes[i], config, j, duration);
+        ctx.clearRect(0, 0, width, height);
+        ctx.save();
+
+        for (let i = 0; i < config.balls.length; i++) {
+
+            ball = config.balls[i];
+            
+            // geometry
+            fire.x = parseInt(ball.x); 
+            fire.y = parseInt(ball.y);
+            fire.width = parseInt(ball.width);
+            fire.height = parseInt(ball.height);
+            fire.pivotX = parseInt(fire.width / 2);
+            fire.pivotY = parseInt(fire.height);
+            fire.rotation = parseInt(ball.rot);
+            
+            // other configuration
+            fire.offset = offset;
+            fire.power = parseFloat(config.power);
+            fire.numPeaks = parseInt(config.peaks);
+            fire.thickness = parseInt(config.lineWidth);
+            fire.shadowBlur = parseInt(config.shadowBlur);
+            fire.shadowColor = config.shadowColor;
+            fire.edgeColor = config.edgeColor;
+            fire.fillColor = config.fillColor;
+
+
+            // draw fireball
+            ctx.save();
+            ctx.lineCap = config.lineCap;
+            ctx.globalCompositeOperation = ball.blend;
+            fire.render(ctx);
+            ctx.restore();
         }
 
         ctx.restore();
+
+        // increment offset
+        offset += offsetStep;
+
+        // write image on disk
         fs.writeFileSync(out, canvas.toBuffer());
     }
 }
